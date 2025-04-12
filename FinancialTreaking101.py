eeb7582c9785aba242d46752d46fc662d5f206be
vinaygau <mgrakeshgautam510@gmail.com>
2025-04-12 23:33:19 +0530

    Update FinancialTreaking101.py

--- FinancialTreaking101.py
+++ FinancialTreaking101.py
@@ -3,7 +3,6 @@ import pandas as pd
 from datetime import datetime, timedelta
 import io
 import time
-from supabase import create_client, Client
 import google.generativeai as genai
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Image
