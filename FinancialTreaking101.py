7e36f2a2aced165e79b37a5595f9397ae9e80ee4
vinaygau <mgrakeshgautam510@gmail.com>
2025-04-13 11:59:01 +0530

    Update FinancialTreaking101.py

--- FinancialTreaking101.py
+++ FinancialTreaking101.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from dateutil.relativedelta import relativedelta
 import numpy as np
+from dotenv import load_dotenv
 import os
 from supabase import create_client
 import google.generativeai as genai
